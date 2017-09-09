8f3b1f8b68b65a2a6e8d026905377f2acf1eaa64
Laurine <peyrardlaurine@gmail.com>
2017-09-09 13:08:20 +0200

    Amélioration globale Plus fun, changement wording Test insta nope

---
+++ index.php
@@ -0,0 +1,157 @@
+
+
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Laurine Peyrard</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Style -->
+		<link href='style.css' rel='stylesheet' type='text/css'>
+
+		<!-- Favicon -->
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/avicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="assets/favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href=assets/favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+		<link rel="manifest" href="assets/favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="assets/favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+
+		<!-- Fonts -->
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Dosis:300,400,500,600,700' rel='stylesheet' type='text/css'>
+		<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600" rel="stylesheet">
+
+		
+	</head>
+
+
+
+	<body>
+		
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous" type="text/javascript"></script>
+		<script type="text/javascript" src="vendor/foundation.min.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-49049061-1', 'auto');
+			ga('send', 'pageview');
+
+		</script>
+
+		<section class="container" id="page1">
+
+			<div id="left"></div>
+			<div id="right"></div>
+
+			<div id="logo" onclick="expand()">
+				<img src="assets/logo.png"/>
+			</div>
+
+		</section>
+
+		<section class="container hidden" id="page2">
+
+			<div class="content">
+
+				<div class="row flex">
+
+					<div id="avatar" class="item">
+						<a href="assets/CV.pdf" target="_blank">
+								<img src="assets/face.png"/>
+								<!--<div>
+									<span id="peekaboo">Télécharger</span>
+									<span>Mon CV</span>
+								</div>-->
+						</a>
+					</div>
+
+					<div class="item">
+						<div id="title">Travaillons ensemble</div>
+						<div id="subtitle">Pour faire du projet de vos rêves une réalité (stylée)</div>		
+					</div>
+					
+					<!--<div class="small-12 medium-4 large-2 columns nopad-right">
+						<div id="face">
+							<a href="assets/CV.pdf" target="_blank">
+								<img src="assets/face.png"/>
+								<div>
+									<span id="peekaboo">Télécharger</span>
+									<span>Mon CV</span>
+								</div>
+							</a>
+						</div>
+					</div>-->
+
+				</div>
+
+				<div class="row">
+					<p>
+					<b>Designer et intégratrice</b> freelance, je conçois et met en place des interfaces web, mais aussi des visuels de communication et du print.
+					</p>
+
+					<p>Je suis en train de coder mon plus beau Portfolio, mais je suis bien occupée par l'organisation du <a href="https://startupweekendannecy.com" target="blank">Startup Weekend Annecy #2 !</a><br/>
+					En attendant, <a href="assets/CV.pdf">téléchargez mon CV</a>, <a href="mailto:peyrardlaurine@gmail.com">envoyez moi un mail</a> ou jetez un oeil à mes réseaux sociaux :)
+					</p>
+
+					
+				</div>
+
+				<div class="row" id="social">
+					<a id="gif" href="mailto:peyrardlaurine@gmail.com">
+						<img src="assets/work.gif" alt="GIF">
+					</a>
+
+					<div>
+						<a href="https://fr.linkedin.com/in/laurinepeyrard" target="_blank" class="tooltipLP icon">
+							<img src="assets/linkedin.png" alt="Icône de Linkedin" />
+							<span class="tooltiptextLP">Profil pro</span>
+						</a>
+						<a href="https://www.instagram.com/laurinepeyrard/" target="_blank" class="tooltipLP icon">
+							<img src="assets/instagram.png" alt="Icône d'Instagram" />
+							<span class="tooltiptextLP">Mon travail</span>
+						</a>
+						<a href="https://medium.com/@peyrardlaurine" target="_blank" class="tooltipLP icon">
+							<img src="assets/medium.png" alt="Icône de Medium" />
+							<span class="tooltiptextLP">Articles drôles</span>
+						</a>
+					</div>
+				</div>
+
+
+			</div>
+		</section>
+
+	</body>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous" type="text/javascript"></script>
+		<script type="text/javascript" src="vendor/spectragram.min.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-49049061-1', 'auto');
+			ga('send', 'pageview');
+
+		</script>
+</html>
--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function expand() {
 	}, 500);
 }
 
+/*
+var Spectra = {
+  instaToken: '5769565501.4269015.e63dbf10bf074a3b8913851e54bdb924',
+  instaID: ' 4269015b902046fd8ce73e7ce2271ef2',
+
+  init: function () {
+    $.fn.spectragram.accessData = {
+      accessToken: this.instaToken,
+      clientID: this.instaID
+    };
+
+    $('.feed').spectragram('getUserFeed',{
+      max: 12,
+      wrapEachWith: '<div class="photo">'
+    });
+  }
+}
+
+Spectra.init();
+*/
+
+
